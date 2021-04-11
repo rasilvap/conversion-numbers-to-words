9bb41b2ed99d1dcc7e04e3c4a36bf776ebf289e5
rodolfosilva <rasilvap22@gmail.com>
2021-04-11 15:50:40 -0500

    feat: (Added initial project Structure).

---
+++ src/main/java/com/numbers/conversion/NumbersConversionToWordsApplication.java
@@ -0,0 +1,13 @@
+package com.numbers.conversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersConversionToWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersConversionToWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numbers/conversion/NumbersConversionToWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numbers.conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersConversionToWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
