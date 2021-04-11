717e1029e9b159360e148fcc74d820324ce5b636
rodolfosilva <rasilvap22@gmail.com>
2021-04-11 16:24:28 -0500

    reactor: (Added test for NumberToWordsServiceImp).

---
+++ src/test/java/com/numbers/conversion/service/impl/NumberToWordsServiceImpTest.java
@@ -0,0 +1,38 @@
+package com.numbers.conversion.service.impl;
+
+import com.numbers.conversion.exception.NumbersToWordsException;
+import com.numbers.conversion.model.dto.NumberDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class NumberToWordsServiceImpTest {
+    NumberToWordsServiceImp service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new NumberToWordsServiceImp();
+    }
+
+    @Test
+    public void testIsMuntantFalse() throws NumbersToWordsException {
+        NumberDTO numberDTO = new NumberDTO(879);
+
+        assertEquals(service.convertNumbersToEnglishWords(numberDTO),"Eight hundred and seventy nine");
+    }
+
+    @Test
+    public void testIsMuntantFalseq() throws NumbersToWordsException {
+        NumberDTO numberDTO = new NumberDTO(9999999999999L);
+
+        NumbersToWordsException thrown = assertThrows(
+                NumbersToWordsException.class,
+                () -> service.convertNumbersToEnglishWords(numberDTO)
+        );
+        assertTrue(thrown.getMessage().contains("Number exceed allowed value."));
+    }
+}
