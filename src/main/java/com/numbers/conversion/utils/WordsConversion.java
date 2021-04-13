6a487f9c16cf6ccfa3b1cab749ab5fa937435a7c
rodolfosilva <rasilvap22@gmail.com>
2021-04-13 17:43:40 -0500

    refactor: refactor of the logic to format the final word, added negative to the final word for negative numbers.

--- src/main/java/com/numbers/conversion/utils/WordsConversion.java
+++ src/main/java/com/numbers/conversion/utils/WordsConversion.java
@@ -3,6 +3,9 @@ package com.numbers.conversion.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static com.numbers.conversion.utils.Constants.NEGATIVE_NUMBER;
+import static com.numbers.conversion.utils.Constants.SEPARATOR;
+
 /**
  * This class contains the methods  to make words transformations.
  */
@@ -15,9 +18,12 @@ public class WordsConversion {
      * @param str
      * @return
      */
-    public static String capitalizeFinalString(String str) {
+    public static String capitalizeAndFormatFinalString(String str, boolean isNegative) {
         if (str == null || str.isEmpty()) {
             return str;
+        }
+        if (isNegative) {
+            str = NEGATIVE_NUMBER + SEPARATOR + str;
         }
         String capitalizedWordsNumber = str.substring(0, 1).toUpperCase() + str.substring(1);
         logger.info("Ending program with capitalized number to words: {}", capitalizedWordsNumber);
